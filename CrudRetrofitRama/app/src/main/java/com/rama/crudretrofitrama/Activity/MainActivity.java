d62f7e7dab10def97d0f745614d49f444ee94d57
ramawidiy7422 <78394142+ramawidiy7422@users.noreply.github.com>
2023-06-27 01:06:57 +0700

    Add files via upload

---
+++ CrudRetrofitRama/app/src/main/java/com/rama/crudretrofitrama/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rama.crudretrofitrama.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rama.crudretrofitrama.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
